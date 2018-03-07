e8bb71871293495afb38d3f2b8dde4a4e6dd3ecb
HarryLu95 <HarryLu95@github.com>
2018-03-06 23:55:09 -0800

    UPDATE

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/thinkpad/Documents/GitHub/dalu0511")
+setwd("/Users/thinkpad/Documents/GitHub/dalu")
 
 #render your sweet site. 
 rmarkdown::render_site()
