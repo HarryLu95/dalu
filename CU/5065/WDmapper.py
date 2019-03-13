21e9e9eaf7c39b4b4d953124fdaf92c33615877c
Da (Harry) Lu <lvda_1@163.com>
2019-03-12 21:58:48 -0400

    5065

--- CU/5065/WDmapper.py
+++ CU/5065/WDmapper.py
@@ -1,17 +1,20 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
+
 # Name: Da Lu
 # ID: dl947
 
 import sys
-
+#give the list of delimiters that need to be removed
 punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 
 for line in sys.stdin:
     no_punc = ""
     line = line.strip()
+    #scan all the characters in the passage and remove every delimiters
     for i in line:
         if i not in punc:
             no_punc = no_punc + i
+    #split the cleaned passage with space, so that every chuck is a word
     words = no_punc.split()
     for word in words:
-        mr.write("%s\t%s\n" % (word, 1))
+        print "%s\t%s\n" % (word, 1)
--- CU/5065/WDreducer.py
+++ CU/5065/WDreducer.py
@@ -1,4 +1,5 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
+
 # Name: Da Lu
 # ID: dl947
 
@@ -8,28 +9,27 @@ word = None
 d = {}
 
 for line in sys.stdin:
-    #去回車
+    #remove "Enter"
     line = line.strip()
-    #用tab分
+    #use "Tab" to split
     current_word, current_count = line.split("\t", 1)
-    #把1從string改成int
+    #change l from string to int
     try:
         current_count = int(current_count)
     except ValueError:
         continue
-    
+    #add the word to the dictionary d
     if d.has_key(current_word) == False:
         d.update({current_word:current_count})
     else:
         temp = d.get(current_word) + 1
         d[current_word] = temp
-
+#get all the words and number counts
 words = d.keys()
 counts = d.values()
 pairs = []
-
-l = len(words)
-for i in range(l):
+#create the output in the instructed format
+for i in range(len(words)):
     w = words[i]
     c = counts[i]
     pairs = pairs + [(words[i], counts[i])]
