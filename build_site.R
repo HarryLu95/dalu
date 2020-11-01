72ba5a32ea7c7e0e1425f57cd482fb1debd3ec47
Da (Harry) Lu <lvda_1@163.com>
2020-10-31 22:19:56 -0400

    Update build_site.R

--- build_site.R
+++ build_site.R
@@ -1,8 +1,7 @@
-#Set our working directory. 
-#This helps avoid confusion if our working directory is 
-#not our site because of other projects we were 
-#working on at the time. 
-setwd("/Users/thinkpad/Documents/GitHub/dalu")
+### Set our working directory.
+# This helps avoid confusion if our working directory is not our
+#   site because of other projects we were working on at the time.
+setwd("C:/Users/Harry Lu's X1/Documents/GitHub/dalu")
 
-#render your sweet site. 
+# render your sweet site.
 rmarkdown::render_site()
