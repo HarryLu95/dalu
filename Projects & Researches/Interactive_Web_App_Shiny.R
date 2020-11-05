d6aad887e4b209cb2e6035e3327225dfdd10edac
Da (Harry) Lu <lvda_1@163.com>
2020-11-05 10:29:00 -0500

    udpate interactive web app

--- Projects & Researches/Interactive_Web_App_Shiny.R
+++ Projects & Researches/Interactive_Web_App_Shiny.R
@@ -1,47 +1,58 @@
+### load libraries
+library(knitr)
+library(kableExtra)
 library(dplyr)
+library(magrittr)
 library(readr)
-library(stats)
+library(ggplot2)
+library(plotly)
 library(shiny)
-gapminder=read.csv("http://bioconnector.org/data/gapminder.csv")
-colnames(gapminder)=c("Country","Continent","Year","Life.Expectancy","Population","GDP.per.Capita")
-UI=fluidPage(
+
+### load data
+gapminder_url = "https://bioconnector.github.io/workshops/data/gapminder.csv"
+gapminder = read_csv(gapminder_url)
+Years = gapminder %>% pull(year) %>% unique()
+
+### UI Design
+UI = fluidPage(
   titlePanel("World Facts"),
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId="select",
-                  label="Choose a country",
-                  choices=unique(gapminder$Country)
+      selectInput(inputId = "select",
+                  label = "Choose a country",
+                  choices = unique(gapminder$country)
       ),
-      selectInput(inputId="object",
-                  label="Choose from the following",
-                  choices=c("Life Expectancy","Population","GDP per Capita")
+      selectInput(inputId = "object",
+                  label = "Choose from the following",
+                  choices = c("Life Expectancy", "Population", "GDP per Capita")
       )
     ),
-    mainPanel(plotOutput(outputId="figure"),
-              tableOutput(outputId="data")
+    mainPanel(plotOutput(outputId = "figure"),
+              tableOutput(outputId = "data")
     )
   )
 )
 
-SERVER=function(input,output){
-  f=function(temp){
-    result=subset(gapminder,gapminder$Country==temp)
+### Server Design
+SERVER = function(input, output){
+  f = function(temp) {
+    result = subset(gapminder, gapminder$country == temp)
     return(result)
   }
-  output$figure=renderPlot({
-    country.name=reactive(input$select)
+  output$figure = renderPlot({
+    country.name = reactive(input$select)
     dat=f(country.name())
     if (input$object=="Life Expectancy"){
-      plot(dat$Life.Expectancy~dat$Year,xlim=c(1950,2010),xlab="Year",ylab="Life Expectancy",lty=2,type="l",main=c("Life Expectancy of ",country.name()))
-      points(dat$Life.Expectancy~dat$Year,pch=19,col=1)
+      plot(dat$lifeExp~dat$year,xlim=c(1950,2010),xlab="Year",ylab="Life Expectancy",lty=2,type="l",main=c("Life Expectancy of ",country.name()))
+      points(dat$lifeExp~dat$year,pch=19,col=1)
     }
     if (input$object=="Population"){
-      plot(dat$Population~dat$Year,xlim=c(1950,2010),xlab="Year",ylab="Population",lty=2,type="l",main=c("Population of ",country.name()))
-      points(dat$Population~dat$Year,pch=19,col=1)
+      plot(dat$pop~dat$year,xlim=c(1950,2010),xlab="Year",ylab="Population",lty=2,type="l",main=c("Population of ",country.name()))
+      points(dat$pop~dat$year,pch=19,col=1)
     }
     if (input$object=="GDP per Capita"){
-      plot(dat$GDP.per.Capita~dat$Year,xlim=c(1950,2010),xlab="Year",ylab="GDP per Capita",lty=2,type="l",main=c("GDP per Capita of ",country.name()))
-      points(dat$GDP.per.Capita~dat$Year,pch=19,col=1)
+      plot(dat$gdpPercap~dat$year,xlim=c(1950,2010),xlab="Year",ylab="GDP per Capita",lty=2,type="l",main=c("GDP per Capita of ",country.name()))
+      points(dat$gdpPercap~dat$year,pch=19,col=1)
     }
   })
   output$data=renderTable(colnames=T,{
@@ -49,20 +60,20 @@ SERVER=function(input,output){
     dat=f(country.name())
     if (input$object=="Life Expectancy"){
       temp=c()
-      temp$Year=dat$Year
-      temp$`Life Expectancy`=dat$Life.Expectancy
+      temp$Year=dat$year
+      temp$`Life Expectancy`=dat$lifeExp
       return(temp)
     }
     if (input$object=="Population"){
       temp=c()
-      temp$Year=dat$Year
-      temp$Population=dat$Population
+      temp$Year=dat$year
+      temp$Population=dat$pop
       return(temp)
     }
     if (input$object=="GDP per Capita"){
       temp=c()
-      temp$Year=dat$Year
-      temp$`GDP per Capita`=dat$GDP.per.Capita
+      temp$Year=dat$year
+      temp$`GDP per Capita`=dat$gdpPercap
       return(temp)
     }
   })
