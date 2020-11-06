a56f8a450d2b35eeb50c3f8302f7ab8916523659
Da (Harry) Lu <lvda_1@163.com>
2020-11-06 01:49:49 -0500

    update interactive web app

--- Projects & Researches/Interactive_Web_App_Shiny.R
+++ Projects & Researches/Interactive_Web_App_Shiny.R
@@ -7,6 +7,7 @@ library(readr)
 library(ggplot2)
 library(plotly)
 library(shiny)
+library(DT)
 
 ### load data
 gapminder_url = "https://bioconnector.github.io/workshops/data/gapminder.csv"
@@ -18,65 +19,118 @@ UI = fluidPage(
   titlePanel("World Facts"),
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = "select",
+      selectInput(inputId = "country_selection_input",
                   label = "Choose a country",
                   choices = unique(gapminder$country)
       ),
-      selectInput(inputId = "object",
+      selectInput(inputId = "metrics_selection_input",
                   label = "Choose from the following",
                   choices = c("Life Expectancy", "Population", "GDP per Capita")
       )
     ),
     mainPanel(plotOutput(outputId = "figure"),
-              tableOutput(outputId = "data")
+              # tableOutput(outputId = "data"),
+              dataTableOutput(outputId = "DT_output")
     )
   )
 )
 
 ### Server Design
 SERVER = function(input, output){
-  f = function(temp) {
-    result = subset(gapminder, gapminder$country == temp)
-    return(result)
+  # helper functions
+  get_curr_country_data = function(input_country_name) {
+    curr_country_data = gapminder %>%
+      filter(country == input_country_name) %>%
+      mutate(year = year %>% as.integer()) %>%
+      mutate(pop = pop %>% as.integer()) %>%
+      mutate(lifeExp = lifeExp %>% round(digits = 1)) %>%
+      mutate(gdpPercap = gdpPercap %>% round(digits = 2)) %>%
+      rename(`Country` = country,
+             `Continent` = continent,
+             `Year` = year,
+             `Life Expectancy` = lifeExp,
+             `Population` = pop,
+             `GDP per Capita` = gdpPercap)
+    return(curr_country_data)
   }
+  
+  # create plot
   output$figure = renderPlot({
-    country.name = reactive(input$select)
-    dat=f(country.name())
-    if (input$object=="Life Expectancy"){
-      plot(dat$lifeExp~dat$year,xlim=c(1950,2010),xlab="Year",ylab="Life Expectancy",lty=2,type="l",main=c("Life Expectancy of ",country.name()))
-      points(dat$lifeExp~dat$year,pch=19,col=1)
+    country_name = reactive(input$country_selection_input)
+    curr_country_data = get_curr_country_data(country_name())
+    
+    p_1 = ggplot(data = curr_country_data, mapping = aes(x = Year, y = `Life Expectancy`)) +
+      geom_point() +
+      geom_line() +
+      ggtitle(paste0("Life Expectancy of ", country_name())) +
+      theme(plot.title = element_text(size = 20, hjust = 0.5)) +
+      scale_x_continuous(labels = Years, breaks = Years)
+    p_2 = ggplot(data = curr_country_data, mapping = aes(x = Year, y = `Population`)) +
+      geom_point() +
+      geom_line() +
+      ggtitle(paste0("Population of ", country_name())) +
+      theme(plot.title = element_text(size = 20, hjust = 0.5)) +
+      scale_x_continuous(labels = Years, breaks = Years)
+    p_3 = ggplot(data = curr_country_data, mapping = aes(x = Year, y = `GDP per Capita`)) +
+      geom_point() +
+      geom_line() +
+      ggtitle(paste0("GDP per Capita of ", country_name())) +
+      theme(plot.title = element_text(size = 20, hjust = 0.5)) +
+      scale_x_continuous(labels = Years, breaks = Years)
+    
+    if (input$metrics_selection_input == "Life Expectancy") {
+      p_1 %>% print()
     }
-    if (input$object=="Population"){
-      plot(dat$pop~dat$year,xlim=c(1950,2010),xlab="Year",ylab="Population",lty=2,type="l",main=c("Population of ",country.name()))
-      points(dat$pop~dat$year,pch=19,col=1)
+    if (input$metrics_selection_input == "Population") {
+      p_2 %>% print()
     }
-    if (input$object=="GDP per Capita"){
-      plot(dat$gdpPercap~dat$year,xlim=c(1950,2010),xlab="Year",ylab="GDP per Capita",lty=2,type="l",main=c("GDP per Capita of ",country.name()))
-      points(dat$gdpPercap~dat$year,pch=19,col=1)
-    }
-  })
-  output$data=renderTable(colnames=T,{
-    country.name=reactive(input$select)
-    dat=f(country.name())
-    if (input$object=="Life Expectancy"){
-      temp=c()
-      temp$Year=dat$year
-      temp$`Life Expectancy`=dat$lifeExp
-      return(temp)
-    }
-    if (input$object=="Population"){
-      temp=c()
-      temp$Year=dat$year
-      temp$Population=dat$pop
-      return(temp)
-    }
-    if (input$object=="GDP per Capita"){
-      temp=c()
-      temp$Year=dat$year
-      temp$`GDP per Capita`=dat$gdpPercap
-      return(temp)
+    if (input$metrics_selection_input == "GDP per Capita") {
+      p_3 %>% print()
     }
   })
+  
+  # create table
+  output$data = renderTable({
+    country_name = reactive(input$country_selection_input)
+    curr_country_data = get_curr_country_data(country_name())
+
+    df_1 = curr_country_data %>% select(Year, `Life Expectancy`)
+    df_2 = curr_country_data %>% select(Year, `Population`)
+    df_3 = curr_country_data %>% select(Year, `GDP per Capita`)
+
+    if (input$metrics_selection_input == "Life Expectancy") {
+      return(df_1)
+    }
+    if (input$metrics_selection_input == "Population") {
+      return(df_2)
+    }
+    if (input$metrics_selection_input == "GDP per Capita") {
+      return(df_3)
+    }
+  })
+  
+  # create datatable
+  output$DT_output = renderDataTable(
+    {
+      country_name = reactive(input$country_selection_input)
+      curr_country_data = get_curr_country_data(country_name())
+      
+      df_1 = curr_country_data %>% select(Year, `Life Expectancy`)
+      df_2 = curr_country_data %>% select(Year, `Population`)
+      df_3 = curr_country_data %>% select(Year, `GDP per Capita`)
+      
+      if (input$metrics_selection_input == "Life Expectancy") {
+        return(df_1)
+      }
+      if (input$metrics_selection_input == "Population") {
+        return(df_2)
+      }
+      if (input$metrics_selection_input == "GDP per Capita") {
+        return(df_3)
+      }
+    },
+    options = list(pageLength = 20, searching = FALSE)
+  )
 }
 
 shinyApp(ui=UI,server=SERVER)
