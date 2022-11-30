100a9e640f2fdb6752aacf2209c3a1ef1fa49ebc
Da (Harry) Lu <dalu.harry95@gmail.com>
2022-11-30 00:24:51 -0500

    Create helpers.py

---
+++ nonpublic_files/helpers.py
@@ -0,0 +1,19 @@
+import json
+
+### read local files
+def load_text_file(file_path, file_mode = "r", file_type = "t", encoding_method = "UTF-8"):
+    with open(file_path, mode = file_mode + file_type, encoding = encoding_method) as f:
+        file = f.readlines()
+    return file
+
+def load_json_file_to_dict(file_path, file_mode = "r", file_type = "t", encoding_method = "UTF-8"):
+    json_file = load_text_file(file_path, file_mode, file_type, encoding_method)
+    j = json.loads("".join(json_file))
+    return j
+
+### write out files
+def write_dict_to_json_file(d, file_path, file_mode = "w", file_type = "t", encoding_method = "UTF-8"):
+    d_string = json.dumps(d, indent = 4)
+    with open(file_path, mode = file_mode + file_type, encoding = encoding_method) as f:
+        f.write(d_string)
+    return
